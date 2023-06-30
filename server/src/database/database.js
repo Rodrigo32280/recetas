47ca3f87d622e532c27d863bc160a3c13a0dc71c
xDamiaann <damian.pilco@espoch.edu.ec>
2023-06-29 20:41:14 -0500

    cambios jorge pilco

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "Aioros0980369953", {
   host: "localhost",
   dialect: "postgres",
 });
