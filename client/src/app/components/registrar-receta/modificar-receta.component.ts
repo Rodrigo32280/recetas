558a5a354372dc30e20cd30d0d50f848ea07448e
Rodrigo90908 <edison-rodrigo01@hotmail.com>
2023-06-29 22:16:04 -0300

    V1

---
+++ client/src/app/components/registrar-receta/modificar-receta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-receta',
+  templateUrl: './modificar-receta.component.html',
+  styleUrls: ['./modificar-receta.component.css']
+})
+export class ModificarRecetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
